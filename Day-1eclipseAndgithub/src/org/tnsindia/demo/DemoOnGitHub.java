43a3060cd4aaa67c1cd9d19397824968a9ba0232
tejaswini <tejaswinirajappa5@gmail.com>
2022-11-25 12:11:36 +0530

    First commit

---
+++ Day-1eclipseAndgithub/src/org/tnsindia/demo/DemoOnGitHub.java
@@ -0,0 +1,11 @@
+package org.tnsindia.demo;
+
+public class DemoOnGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
