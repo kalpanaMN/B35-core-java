bab3c7eef23355d38f686970febf2dd7804b8e37
tejaswini <tejaswinirajappa5@gmail.com>
2022-11-25 12:23:46 +0530

    first commit

---
+++ Day-2DataTypes,variablesAndIo/src/org/tnsindia/datatypedemo/DemoOnDatatype.java
@@ -0,0 +1,12 @@
+package org.tnsindia.datatypedemo;
+
+public class DemoOnDatatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNum=5;
+		System.out.println(myNum);
+
+	}
+
+}
