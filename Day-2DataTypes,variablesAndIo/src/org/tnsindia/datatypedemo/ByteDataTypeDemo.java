bacba542902daf2d982f8afe82fe6df243192634
tejaswini <tejaswinirajappa5@gmail.com>
2022-11-25 12:59:23 +0530

    first commit

---
+++ Day-2DataTypes,variablesAndIo/src/org/tnsindia/datatypedemo/ByteDataTypeDemo.java
@@ -0,0 +1,30 @@
+package org.tnsindia.datatypedemo;
+
+public class ByteDataTypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int xyz=120;
+	      long abc=2147483648L;
+	    	  System.out.println("the value is:"+xyz);
+	    	  System.out.println("the value is:"+abc);
+	    	  char pqr='z';
+	    	  float c=12.89f;
+	    	  double d=41.6;
+	    	  String str="kalpana";
+	    	  @SuppressWarnings("unused")
+	  		boolean var=true;
+	      	  int x=(int)12.5;
+	      	  float y=(float)34;
+	      	  System.out.println("the value is:"+pqr);
+	      	  System.out.println("the value is:"+c);
+	      	  System.out.println("the value is:"+d);
+	      	  System.out.println("the value is:"+x);
+	      	  System.out.println("the value is:"+y);
+	      	  System.out.println("the value is:"+str);
+	      	  
+	        
+	  	}
+}
+
+	
---
+++ Day-2DataTypes,variablesAndIo/src/org/tnsindia/datatypedemo/SingleInheritanceExcution.java
@@ -0,0 +1,24 @@
+package org.tnsindia.datatypedemo;
+import java.util.Scanner;
+
+public class SingleInheritanceExcution {
+	public static void main(String[] args) {
+		System.out.println("enter the name of the animal:");
+		Scanner s=new Scanner(System.in);
+		String name1=s.nextLine();
+		System.out.println(" enter the name of the animal:");
+		String breed1=s.nextLine();
+      dog d=new dog();
+        d.name=name1;
+        d.breed=breed1;
+        d.display();
+        d.print();
+        s.close();
+	}
+
+}
+
+	
+	
+
+
