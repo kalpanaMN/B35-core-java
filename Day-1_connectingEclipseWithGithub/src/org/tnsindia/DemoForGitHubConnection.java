a5ce01484208c632c45176efdb33ca64908dae16
kalpana <kalpana@kalpanamn756>
2022-12-25 23:15:57 +0530

    first commit

---
+++ Day-1_connectingEclipseWithGithub/src/org/tnsindia/DemoForGitHubConnection.java
@@ -0,0 +1,14 @@
+package org.tnsindia;
+
+//Demo on to connect eclipse with GitHub
+
+/*...1..Create a class and print a  statement..*/
+
+public class DemoForGitHubConnection {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Welcome to TNS India Foundation");
+	}
+
+}
